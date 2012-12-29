09a08e7fa109a1d8dfa16837488115194c595b16
Leanfear <stefan.original@gmx.at>
2012-12-29 16:57:18 +0100

    Update Laxy/PanelDB.cs

--- Laxy/PanelDB.cs
+++ Laxy/PanelDB.cs
@@ -10,7 +10,7 @@ namespace Laxy
     {
         public PanelDB()
         {
-            this.DoubleBuffered = true;
+            this.DoubleBuffered = true; // browser-editet xyz
         }
     }
 }
